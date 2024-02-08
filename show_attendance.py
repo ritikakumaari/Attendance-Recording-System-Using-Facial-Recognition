57667102e80eb93e9d49a68a9dce217f948457d8
Khushi Bansal <khushib938@gmail.com>
2024-02-08 21:34:12 +0530

    Added calculate_attendance function

--- show_attendance.py
+++ show_attendance.py
@@ -6,3 +6,25 @@ import csv
 import tkinter as tk
 from tkinter import *
 
+def subjectchoose(text_to_speech):
+    def calculate_attendance():
+        Subject = tx.get()
+        if Subject == "":
+            t = 'Please enter the subject name.'
+            text_to_speech(t)
+        os.chdir(
+
+        )
+        filenames = glob(
+
+        )
+        df = [pd.read_csv(f) for f in filenames]
+        newdf = df[0]
+        for i in range(1, len(df)):
+            newdf = newdf.merge(df[i], how="outer")
+        newdf.fillna(0, inplace=True)
+        newdf["Attendance"] = 0
+        for i in range(len(newdf)):
+            newdf["Attendance"].iloc[i] = str(int(round(newdf.iloc[i, 2:-1].mean()*100)))+ '%'
+
+
